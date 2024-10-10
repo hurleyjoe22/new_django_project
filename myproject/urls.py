40a07fe7388bdf9ebe0edf526473f1d805c928c2
hurleyjoe22 <brandongunn123@hotmail.com>
2024-10-10 23:16:37 +1100

    Add Hello, World! homepage

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views  # Import the views from your app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
