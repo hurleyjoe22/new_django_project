ce462cf31c3af1b4719f3e4ce1c8c6f0e85d1437
hurleyjoe22 <brandongunn123@hotmail.com>
2024-10-11 12:42:58 +1100

    Add Hello, World! HTML template

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,4 @@
-
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, World!")
-
-
-# Create your views here.
+    return render(request, 'home.html')
